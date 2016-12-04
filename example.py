4a3db668be9835e974bf19f8d7a3a5c413b5ae8e
Marco Rougeth <marco@rougeth.com>
2016-12-04 20:43:46 -0200

    Refactor Thumbot to have a better api

--- example.py
+++ example.py
@@ -4,20 +4,18 @@ from thumbot import Thumbot
 
 TOKEN = 'YOUR BOT TOKEN'
 bot = telebot.TeleBot(TOKEN)
-thumbot = Thumbot()
 
 
 @bot.message_handler()
 def handle_all_messages(message):
-    thumbot.reset()
     message = bot.send_message(message.chat.id, 'Awesome message...',
-                               reply_markup=thumbot.keyboard())
+                               reply_markup=Thumbot.empty_keyboard())
 
 
 @bot.callback_query_handler(lambda q: q.data == 'thumb_up')
 def thumb_up(callback):
-    thumbot.check(callback.message)
-    if thumbot.add_thumb_up(callback.from_user.id):
+    thumbot = Thumbot(callback.message)
+    if thumbot.up(callback.from_user.id):
         bot.edit_message_reply_markup(
             chat_id=callback.message.chat.id,
             message_id=callback.message.message_id,
@@ -26,8 +24,8 @@ def thumb_up(callback):
 
 @bot.callback_query_handler(lambda q: q.data == 'thumb_down')
 def thumb_down(callback):
-    thumbot.check(callback.message)
-    if thumbot.add_thumb_down(callback.from_user.id):
+    thumbot = Thumbot(callback.message)
+    if thumbot.down(callback.from_user.id):
         bot.edit_message_reply_markup(
             chat_id=callback.message.chat.id,
             message_id=callback.message.message_id,
