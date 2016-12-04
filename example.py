a7e222acecaed8a7c7565e229bfd5bc09411b522
Marco Rougeth <marco@rougeth.com>
2016-12-04 19:37:53 -0200

    First concrete version

--- example.py
+++ example.py
@@ -11,10 +11,30 @@ thumbot = Thumbot()
 
 @bot.message_handler()
 def handle_all_messages(message):
-    thumbot.check(message)
-
-    message = bot.send_message(message.from_user.id, 'Awesome message...',
+    thumbot.reset()
+    message = bot.send_message(message.chat.id, 'Awesome message...',
                                reply_markup=thumbot.keyboard())
 
 
-bot.polling()
+@bot.callback_query_handler(lambda q: q.data == 'thumb_up')
+def thumb_up(callback):
+    thumbot.check(callback.message)
+    if thumbot.add_thumb_up(callback.from_user.id):
+        bot.edit_message_reply_markup(
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            reply_markup=thumbot.keyboard())
+
+
+@bot.callback_query_handler(lambda q: q.data == 'thumb_down')
+def thumb_down(callback):
+    thumbot.check(callback.message)
+    if thumbot.add_thumb_down(callback.from_user.id):
+        bot.edit_message_reply_markup(
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            reply_markup=thumbot.keyboard())
+
+
+if __name__ == '__main__':
+    bot.polling()
--- thumbot.py
+++ thumbot.py
@@ -2,26 +2,29 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pymongo import MongoClient
 
 
-client = MongoClient()
-db = client.messages
-
-
 class Thumbot:
 
     THUMBS_UP_EMOJI = '\U0001F44D'
     THUMBS_DOWN_EMOJI = '\U0001F44E'
 
+    def __init__(self):
+        client = MongoClient()
+        self.db = client.thumbot
+
+        self.ups = []
+        self.downs = []
+
     @property
     def up_label(self):
         if self.ups:
-            return '{} {}'.format(self.THUMBS_UP_EMOJI, self.ups)
+            return '{} {}'.format(self.THUMBS_UP_EMOJI, len(self.ups))
 
         return self.THUMBS_UP_EMOJI
 
     @property
     def down_label(self):
         if self.downs:
-            return '{} {}'.format(self.THUMBS_DOWN_EMOJI, self.downs)
+            return '{} {}'.format(self.THUMBS_DOWN_EMOJI, len(self.downs))
 
         return self.THUMBS_DOWN_EMOJI
 
@@ -33,9 +36,9 @@ class Thumbot:
 
         return keyboard.row(*buttons)
 
-    def keyboard(self):
-        up_button = self._create_button(self.up_label, 'ups')
-        down_button = self._create_button(self.down_label, 'downs')
+    def keyboard(self, up_label=None, down_label=None):
+        up_button = self._create_button(self.up_label, 'thumb_up')
+        down_button = self._create_button(self.down_label, 'thumb_down')
         return self._create_keyboard(up_button, down_button)
 
     def check(self, message):
@@ -43,20 +46,61 @@ class Thumbot:
         self.chat_id = message.from_user.id
         self.message_id = message.message_id
 
-        message = db.find_one({
+        message = self.db.messages.find_one({
             'chat_id': self.chat_id,
             'message_id': self.message_id,
         })
 
         if not message:
-            object_id = db.insert_one({
+            object_id = self.db.messages.insert_one({
                 'chat_id': self.chat_id,
                 'message_id': self.message_id,
-                'ups': [],
-                'downs': [],
+                'ups': self.ups,
+                'downs': self.downs,
             }).inserted_id
 
-            message = db.find_one({'_id': object_id})
+            message = self.db.messages.find_one({'_id': object_id})
 
         self.ups = message['ups']
         self.downs = message['downs']
+
+    def update(self):
+        self.db.messages.update_one(
+            {
+                'chat_id': self.chat_id,
+                'message_id': self.message_id,
+            },
+            {
+                '$set': {
+                    'ups': self.ups,
+                    'downs': self.downs
+                }
+            }
+        )
+
+    def add_thumb_up(self, user_id):
+        if user_id in self.ups:
+            return False
+
+        self.ups.append(user_id)
+        if user_id in self.downs:
+            self.downs.remove(user_id)
+
+        self.update()
+        return True
+
+    def add_thumb_down(self, user_id):
+        if user_id in self.downs:
+            return False
+
+        self.downs.append(user_id)
+        if user_id in self.ups:
+            self.ups.remove(user_id)
+        self.update()
+
+        return True
+
+    def reset(self):
+        self.chat_id = self.message_id = ''
+        self.ups = []
+        self.downs = []
